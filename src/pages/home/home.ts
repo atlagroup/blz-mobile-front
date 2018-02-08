9ef72829e51976af0216dee52659f80e8165e178
saviocamacam <saviocamacam@alunos.utfpr.edu.br>
2018-02-08 18:32:35 -0200

    Página do Home configurada para Exibir as listas de abastecimentos e manutenções, bem como os botões para cadastrar os mesmos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,59 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  information: string = "fuel";
+
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController
+  ) {
+
+  }
+
+  newFuel() {
+    let alert = this.alertCtrl.create({
+      title: 'Cadastrar Abastecimento',
+      inputs: [
+        {
+          name: 'data',
+          placeholder: 'Data'
+        },
+        {
+          name: 'km',
+          placeholder: 'KM'
+        },
+        {
+          name: 'type',
+          placeholder: 'Tipo'
+        },
+        {
+          name: 'value',
+          placeholder: 'Valor'
+        },
+        {
+          name: 'gty',
+          placeholder: 'Quantidade'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Confirmar',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  newMaintenance() {
 
   }
 
