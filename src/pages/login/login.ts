59456b1b28b2515862651ace9f1f18c961aedde8
jeffmant <jmantovani@alunos.utfpr.edu.br>
2018-02-08 18:36:11 -0200

    LoginPage, FirebaseConfig

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,24 +2,31 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the HomePage page.
+ * Generated class for the LoginPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
 
+import { HomePage } from '../home/home';
+
 @IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
+  selector: 'page-login',
+  templateUrl: 'login.html',
 })
-export class HomePage {
+export class LoginPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+    console.log('ionViewDidLoad LoginPage');
   }
+
+  pushHomePage(){
+    this.navCtrl.push(HomePage);
+  }
+
 
 }
